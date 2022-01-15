9cadbb8316723cc5b9a17affa7aa8b77d0aa8ef8
AndreiaJM <andreia.ajms@gmail.com>
2022-01-15 17:53:16 -0300

    remover imports não usados

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,7 +16,7 @@ function MovieCard() {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore/>
 
                 <Link to={`/form/${movie.id}`}><div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
